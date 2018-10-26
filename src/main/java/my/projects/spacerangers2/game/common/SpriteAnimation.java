b3e6ff148d20481456ab2331d1690478b10233b5
Tordek <tordektopor@gmail.com>
2018-10-27 00:15:59 +0300

    Static builder method was created in class SpriteAnimation

--- src/main/java/my/projects/spacerangers2/game/common/SpriteAnimation.java
+++ src/main/java/my/projects/spacerangers2/game/common/SpriteAnimation.java
@@ -3,12 +3,28 @@ package my.projects.spacerangers2.game.common;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import my.projects.resources.ImagesBuilder;
 
 public class SpriteAnimation {
 	private Image[] images;
 	private int currentImg;
 	private int imageCount;
 	private ImageView window;
+	
+	public static SpriteAnimation explosionSA() {
+		ImagesBuilder ib = ImagesBuilder.getInstance();
+		return new SpriteAnimation(ib.getExplosionImages(),0);
+	}
+	
+	public static SpriteAnimation asteroidSA() {
+		ImagesBuilder ib = ImagesBuilder.getInstance();
+		return new SpriteAnimation(ib.getAsteroidImages());
+	}
+	
+	public static SpriteAnimation userShipSA() {
+		ImagesBuilder ib = ImagesBuilder.getInstance();
+		return new SpriteAnimation(ib.getUserShipImages());
+	}
 	
 	public SpriteAnimation() {
 		images = null;
